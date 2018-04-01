9b2d0727c7ba683d7c429be93aa6e3908e2ba4a1
Ernest Micklei <>
2018-04-01 16:17:33 +0200

    consume destination + add random delay messages

--- example/main.go
+++ example/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"log"
+	"math/rand"
+	"os"
 	"time"
 
+	"cloud.google.com/go/pubsub"
 	parcello "github.com/emicklei/parcello/v1"
 	"google.golang.org/grpc"
 )
@@ -15,19 +20,47 @@ func main() {
 		log.Fatal("Dial failed:", err)
 	}
 	defer conn.Close()
+
+	go drainDestination()
+
 	client := parcello.NewDeliveryServiceClient(conn)
 
-	after := time.Now().Add(1 * time.Minute)
-	in := new(parcello.DeliverRequest)
-	in.Envelope = &parcello.Envelope{
-		Payload:          []byte("parcello " + time.Now().String()),
-		DestinationTopic: "parcello_destination",
-		UndeliveredTopic: "parcello_undelivered",
-		PublishAfter:     &parcello.Timestamp{Seconds: uint64(after.Unix())},
+	for i := 0; i < 100; i++ {
+		d := time.Duration(rand.Intn(10)) * time.Minute
+		after := time.Now().Add(d)
+		in := new(parcello.DeliverRequest)
+		in.Envelope = &parcello.Envelope{
+			Payload:          []byte(after.UTC().String()),
+			DestinationTopic: "parcello_destination",
+			UndeliveredTopic: "parcello_undelivered",
+			PublishAfter:     &parcello.Timestamp{Seconds: uint64(after.Unix())},
+		}
+		out, err := client.Deliver(context.Background(), in)
+		if err != nil {
+			log.Fatal("Deliver failed:", err)
+		}
+		log.Printf("%#v", out)
 	}
-	out, err := client.Deliver(context.Background(), in)
+	fmt.Print("enter to exit ... ")
+	bufio.NewReader(os.Stdin).ReadString('\n')
+}
+
+func drainDestination() {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "philemonworks")
 	if err != nil {
-		log.Fatal("Deliver failed:", err)
+		log.Fatalf("failed to create PubSub client: %v", err)
 	}
-	log.Printf("%#v", out)
+	sub := client.Subscription("parcello_destination")
+	err = sub.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
+		log.Printf(`id:%s
+	ps published:%s
+	given  after:%s
+	actual after:%s
+`, msg.ID, msg.PublishTime.UTC().String(), string(msg.Data), time.Now().UTC().String())
+		msg.Ack()
+	})
+	if err != nil {
+		log.Printf("Receiving stopped with error %v", err)
+	}
 }
